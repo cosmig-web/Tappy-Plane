b8762de5eb9e1664b95afd40cf42753ebb8444a7
Kowalski65 <gabija.godlauskaite@vilnius.eu>
2024-09-30 17:48:15 +0300

    Planes Start

---
+++ Assets/Scripts/Fly.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : MonoBehaviour
+{
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButton(0))
+        {
+
+        }
+    }
+}
